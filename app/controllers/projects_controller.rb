0d335c873165c025ebf47aed4de0df2ae591fcce
Jim Urbas <git@jimu.net>
2021-08-29 11:49:26 -0500

    Added basic authentication to projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,7 @@
 class ProjectsController < ApplicationController
 
+  http_basic_authenticate_with name: "jimu", password: "secret"
+
   def index
     @projects = Project.all
     render json: @projects
